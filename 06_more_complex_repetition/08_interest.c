8722bda8e02686648fa40cf6311741912ed29cd8
#includestdio.h <mauryaanurag375@gmai.com>
2021-08-29 20:24:24 +0530

    29/8/2021

---
+++ 06_more_complex_repetition/08_interest.c
@@ -0,0 +1,1 @@
+//just take 10 value of p,q,n,q using for loop and print value of a
--- 06_more_complex_repetition/11_hour.c
+++ 06_more_complex_repetition/11_hour.c
@@ -1,12 +1,1 @@
-#include<stdio.h>
-int main()
-{
-    //book
-    {
-int i ;
-for ( i = 1 ; i <= 5 ; printf ( "%d\n", i ) ) ;
-i++ ;
-return 0 ;
-}
-    return 0;
-}
+//in book 
